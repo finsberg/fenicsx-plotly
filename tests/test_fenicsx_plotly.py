60aa577eab18c3374b4d4544086a54143dcbe011
Henrik Finsberg <henrikfinsberg@hotmail.com>
2022-12-27 10:28:07 +0100

    Add support for plotting vector functions

--- tests/test_fenicsx_plotly.py
+++ tests/test_fenicsx_plotly.py
@@ -3,6 +3,7 @@ import itertools as it
 import dolfinx
 import numpy as np
 import pytest
+import ufl
 from fenicsx_plotly import plot
 from mpi4py import MPI
 
@@ -36,9 +37,9 @@ def test_plot_mesh(dim, wireframe):
     "dim, wireframe, degree",
     it.product([2, 3], [True, False], [1, 2, 3]),
 )
-def test_plot_scalar_cg_function_space(dim, wireframe, degree):
+def test_plot_scalar_P_function_space(dim, wireframe, degree):
     mesh = get_mesh(dim)
-    V = dolfinx.fem.FunctionSpace(mesh, ("CG", degree))
+    V = dolfinx.fem.FunctionSpace(mesh, ("P", degree))
     plot(V, wireframe=wireframe, show=False)
 
 
@@ -46,9 +47,63 @@ def test_plot_scalar_cg_function_space(dim, wireframe, degree):
     "dim, wireframe, scatter, degree",
     it.product([2, 3], [True, False], [True, False], [1, 2]),
 )
-def test_plot_scalar_cg_function(dim, wireframe, scatter, degree):
+def test_plot_scalar_P_function(dim, wireframe, scatter, degree):
     mesh = get_mesh(dim)
-    V = dolfinx.fem.FunctionSpace(mesh, ("CG", degree))
+    V = dolfinx.fem.FunctionSpace(mesh, ("P", degree))
     p = dolfinx.fem.Function(V)
     p.interpolate(lambda x: np.sin(x[0]))
     plot(p, scatter=scatter, wireframe=wireframe, show=False)
+
+
+@pytest.mark.parametrize(
+    "dim, wireframe, degree",
+    it.product([2, 3], [True, False], [1, 2, 3]),
+)
+def test_plot_vector_P_function_space(dim, wireframe, degree):
+    mesh = get_mesh(dim)
+    el = ufl.VectorElement("P", mesh.ufl_cell(), degree)
+    V = dolfinx.fem.FunctionSpace(mesh, el)
+    plot(V, wireframe=wireframe, show=False)
+
+
+@pytest.mark.parametrize(
+    "dim, wireframe, norm, normalize, degree, component",
+    it.product(
+        [2, 3],
+        [True, False],
+        [True, False],
+        [True, False],
+        [1, 2],
+        [None, "magnitude", "x", "y", "z"],
+    ),
+)
+def test_plot_vector_cg_function(dim, wireframe, norm, normalize, degree, component):
+    mesh = get_mesh(dim)
+    el = ufl.VectorElement("P", mesh.ufl_cell(), degree)
+    V = dolfinx.fem.FunctionSpace(mesh, el)
+    u = dolfinx.fem.Function(V)
+
+    x = ufl.SpatialCoordinate(mesh)
+
+    if dim == 2:
+        expr = dolfinx.fem.Expression(
+            ufl.as_vector((1 + x[0], x[1])),
+            V.element.interpolation_points(),
+        )
+        if component == "z":
+            return
+    else:
+        expr = dolfinx.fem.Expression(
+            ufl.as_vector((1 + x[0], x[1], x[2])),
+            V.element.interpolation_points(),
+        )
+
+    u.interpolate(expr)
+    plot(
+        u,
+        norm=norm,
+        wireframe=wireframe,
+        show=False,
+        component=component,
+        normalize=normalize,
+    )
